6c648f2d4a619634c9f0146a02e3337ce10c74e2
EvanCapgemini <evan.elijah-mendonsa@capgemini.com>
2025-08-14 10:35:37 +0530

    Add files via upload

---
+++ TestingAssignment2/login.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+if (isset($_POST['cancel'])) {
+    header("Location: index.php");
+    return;
+}
+
+$salt = 'XyZzy12*_';
+$stored_hash = hash('md5', $salt . 'php123');
+
+if (isset($_POST['email']) && isset($_POST['pass'])) {
+    if (strlen($_POST['email']) < 1 || strlen($_POST['pass']) < 1) {
+        $_SESSION['error'] = "Email and password are required";
+        header("Location: login.php");
+        return;
+    } elseif (strpos($_POST['email'], '@') === false) {
+        $_SESSION['error'] = "Email must have an at-sign (@)";
+        header("Location: login.php");
+        return;
+    } else {
+        $check = hash('md5', $salt . $_POST['pass']);
+        if ($check == $stored_hash) {
+            error_log("Login success " . $_POST['email']);
+            $_SESSION['name'] = $_POST['email'];
+            header("Location: view.php");
+            return;
+        } else {
+            error_log("Login fail " . $_POST['email'] . " $check");
+            $_SESSION['error'] = "Incorrect password";
+            header("Location: login.php");
+            return;
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Evan Elijah Mendonsa's Autos Database</title>
+</head>
+<body>
+
+<h1>Please Log In</h1>
+<?php
+if (isset($_SESSION['error'])) {
+    echo('<p style="color: red;">' . htmlentities($_SESSION['error']) . "</p>");
+    unset($_SESSION['error']);
+}
+?>
+<form method="POST">
+    <label for="email">User Name</label>
+    <input type="text" name="email" id="email"><br/>
+    <label for="pass">Password</label>
+    <input type="password" name="pass" id="pass"><br/>
+    <input type="submit" value="Log In">
+    <input type="submit" name="cancel" value="Cancel">
+</form>
+<!-- Hint: The password is php123 -->
+
+</body>
+</html>
