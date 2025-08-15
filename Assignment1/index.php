24ced062ebee020d35907e0c00296ad9fecbd3c1
sect8 <141134831+sect8@users.noreply.github.com>
2025-08-15 17:07:43 +0530

    Add files via upload

---
+++ Assignment1/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Evan Elijah Mendonsa</title>
+</head>
+<body>
+<h1>Welcome to the Autos Database</h1>
+<p><a href="login.php">Please Log In</a></p>
+<p>Attempting to access <a href="autos.php">autos.php</a> without logging in should fail.</p>
+</body>
+</html>
