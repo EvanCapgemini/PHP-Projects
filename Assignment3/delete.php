e56bc1a9998e4ef5a36c5c17e5952fbbd854218e
sect8 <141134831+sect8@users.noreply.github.com>
2025-08-15 17:09:40 +0530

    Add files via upload

---
+++ Assignment3/delete.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require_once "pdo.php";
+if (!isset($_SESSION['name'])) {
+    die("ACCESS DENIED");
+}
+if (!isset($_GET['autos_id'])) {
+    die("Missing autos_id");
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['delete'])) {
+        $stmt = $pdo->prepare("DELETE FROM autos WHERE autos_id = :id");
+        $stmt->execute([':id' => $_GET['autos_id']]);
+        $_SESSION['success'] = "Record deleted";
+        header("Location: index.php");
+        return;
+    } else {
+        header("Location: index.php");
+        return;
+    }
+}
+?>
+<html>
+<head><title>Evan Elijah Mendonsa</title></head>
+<body>
+<h1>Confirm Deletion</h1>
+<form method="POST">
+<input type="submit" name="delete" value="Delete">
+<input type="submit" name="cancel" value="Cancel">
+</form>
+</body>
+</html>
